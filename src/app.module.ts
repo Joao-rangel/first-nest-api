2e02bf8f42e3865f08d54f7679981aec35f16e54
Joao Rangel <joao.vran@gmail.com>
2021-02-11 06:06:40 -0300

    start mysql and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'mysql',
+      url: process.env.DATABASE_URL,
+      entities: [],
+      synchronize: process.env.NODE_ENV === 'development' ? true : false,
+    }),
   imports: [],
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
