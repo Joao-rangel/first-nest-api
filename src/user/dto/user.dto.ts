d4797f4743cce2af04205304baa20d55b1260e55
Joao Rangel <joao.vran@gmail.com>
2021-02-12 17:36:53 -0300

    add class validation

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,11 +1,31 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsOptional,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
   name: string;
 
+  @IsEmail()
+  @IsOptional()
+  @MaxLength(50)
   email?: string;
 }
 
 export class UpdateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
   name?: string;
 
+  @IsEmail()
+  @IsOptional()
+  @MaxLength(50)
   email?: string;
 }
